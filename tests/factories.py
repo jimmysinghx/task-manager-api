361393d3fa706ca996ab19042da4e962eda77e80
jimmysinghx <jimmysingh030704@gmail.com>
2026-03-13 01:23:14 +0530

    Add TaskFactory

--- tests/factories.py
+++ tests/factories.py
@@ -1,7 +1,9 @@
 import factory
 from app.models.user import User 
+from app.models.task import Task
 from faker import Faker
 from app.extensions import bcrypt
+from datetime import datetime , timezone
 
 fake = Faker()
 
@@ -13,3 +15,16 @@ class UserFactory(factory.alchemy.SQLAlchemyModelFactory):
     username = factory.LazyFunction(lambda  : fake.user_name())
     email  = factory.LazyFunction(lambda : fake.email())
     password_hash = factory.LazyFunction(lambda : bcrypt.generate_password_hash("password123").decode("utf-8"))
+
+class TaskFactory(factory.alchemy.SQLAlchemyModelFactory):
+
+    class Meta :
+        model = Task
+        sqlalchemy_session = None
+
+    title = factory.LazyFunction(lambda : fake.sentence(nb_words=4))
+    description = factory.LazyFunction(lambda : fake.sentence(nb_words=10))
+    completed = factory.LazyFunction(lambda : fake.boolean())
+    created_at = factory.LazyFunction(lambda : datetime.now(timezone.utc))
+    user = factory.SubFactory(UserFactory)
+    user_id = factory.LazyAttribute(lambda obj : obj.user.id)
